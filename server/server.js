0ef0d14c26052036ec24488f215e945a8b8cfa73
osborne7 <eleanor.c.osborne@gmail.com>
2020-06-22 22:29:17 -0400

    Begin express lab

---
+++ server/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+app.use((req, res, next) => {
+    console.log(req.originalUrl);
+    next();
+});
+
+// app.use((req,res, next) => {
+//     fs.appendFile()
+// })
+
+// app.get('/', (req, res) => {
+//     res.send('Hello from the web server side.');
+// })
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+let contactArray = [];
+
+app.post('/contact-form', (req, res) => {
+    let name = req.body.name;
+    let email = req.body.email;
+    
+    console.log(`Name: ${name}`);
+    console.log(`Email: ${email}`);
+
+    contactArray.push({
+        name: name,
+        email: email
+    });
+
+    fs.writeFileSync('contacts.json', JSON.stringify(contactArray, null, 2), 'utf-8', err => {
+        if (err) console.log(err);
+    })
+    res.send('Thanks for submitting!');
+})
+
+app.post('/formsubmissions', (req, res) => {
+    fs.readFileSync('contacts.json', { encoding: 'utf-8' }, (err, data) => {
+        if(err) console.log(err);
+        res.send(data);
+    })
+})
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.listen(4114);
